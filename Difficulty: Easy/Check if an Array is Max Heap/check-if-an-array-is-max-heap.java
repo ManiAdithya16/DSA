06a21326313750230330bbcb9e6da29aca79ce67
ManiAdithya16 <dasikamanidhara975@gmail.com>
2026-05-01 00:46:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Check if an Array is Max Heap/check-if-an-array-is-max-heap.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isMaxHeap(int[] arr) {
+
+        int n = arr.length;
+
+        // Only need to check till last parent node
+        for (int i = 0; i <= (n - 2) / 2; i++) {
+
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+
+            // Check left child
+            if (left < n && arr[i] < arr[left]) {
+                return false;
+            }
+
+            // Check right child
+            if (right < n && arr[i] < arr[right]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
