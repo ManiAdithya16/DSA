89233847904579b61fdf18884763b6f907b92603
ManiAdithya16 <dasikamanidhara975@gmail.com>
2026-05-05 10:11:19 +0530

    Add: meeting-schedule - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule/submission-0.java
@@ -0,0 +1,27 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean canAttendMeetings(List<Interval> intervals) {
+        if(intervals.size() == 0){
+            return true;
+        }
+        Collections.sort(intervals, (a, b) -> Integer.compare(a.start, b.start));
+        int lastend = intervals.get(0).end;
+        for(int i=1;i<intervals.size();i++){
+            if(intervals.get(i).start < lastend){
+                return false;
+            }
+            lastend = intervals.get(i).end;
+        }
+        return true;
+    }
+}
